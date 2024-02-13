cfc1f770ba1bdf7e6cf24b500c30562cc33eedd1
Jason Montleon <jmontleo@redhat.com>
2024-02-12 23:09:44 -0500

    Add tests to ensure conf files are processed correctly (#26)
    Signed-off-by: Jason Montleon <jmontleo@redhat.com>

--- kai-service/tests/kai_tests.py
+++ kai-service/tests/kai_tests.py
@@ -33,10 +33,37 @@ async def test_set_value(cli):
                                                       "model_template": "gpt"}""",
     )
 
-    print(await resp.read())
-    print(os.environ.get("KAI_CONFIG"))
     assert resp.status == 200
     assert (
         await resp.text()
         == '{"generated_prompt": "My Prompt description go my original code my solved example my current issue code"}'
     )
+
+
+@pytest.mark.asyncio
+async def test_set_value2(cli):
+    """Test a second template renders properly."""
+    resp = await cli.post(
+        "/generate_prompt",
+        headers={"Content-Type": "application/json"},
+        data=b"""{"issue_description": "description",
+                                                      "language": "go",
+                                                      "example_original_code": "my original code",
+                                                      "example_solved_code": "my solved example",
+                                                      "current_original_code": "my current issue code",
+                                                      "model_template": "alpaca"}""",
+    )
+
+    assert resp.status == 200
+    assert (
+        await resp.text()
+        == '{"generated_prompt": "My Alpaca Prompt description go my original code my solved example my current issue code"}'
+    )
+
+
+def test_model_len():
+    """Test how many models are loaded"""
+    t = kai.load_templates()
+    assert len(t) == 2
+    assert "gpt" in t
+    assert "alpaca" in t
