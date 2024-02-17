31d21000d11acc22beedf5fbc38ebd59df8a4219
John Matthews <jwmatthews@gmail.com>
2024-02-17 10:17:33 -0500

    Added a zero shot and few shot example
    Note, there is an issue with calculating a diff for the solved example
    which needs to be solved in a follow up PR
    
    Signed-off-by: John Matthews <jwmatthews@gmail.com>

--- kai/scm.py
+++ kai/scm.py
@@ -1,31 +1,34 @@
-__all__ = ['GitDiff']
+__all__ = ["GitDiff"]
 
 import logging
+
 from git import BadName, Repo
+
+
 class GitDiff:
     def __init__(self, repo_path):
         self.repo_path = repo_path
-        self.repo = Repo(self.repo_path) 
+        self.repo = Repo(self.repo_path)
 
     def get_patch(self, start_commit_id, end_commit_id="HEAD"):
         # If either commit_id is not valid, this will raise a BadName exception
         start_commit = self.repo.commit(start_commit_id)
         end_commit = self.repo.commit(end_commit_id)
-        return start_commit.diff(end_commit_id, create_patch=True) 
-        
+        return start_commit.diff(end_commit_id, create_patch=True)
+
     def get_patch_for_file(self, start_commit_id, end_commit_id, file_path):
         diff_indexes = self.get_patch(start_commit_id, end_commit_id)
         # We need to search through the indexes to find the diff for our file_path
         patch = None
         for diff in diff_indexes:
-            #print(f"'{file_path}' '{diff.a_path}' '{diff.b_path}' ")
+            # print(f"'{file_path}' '{diff.a_path}' '{diff.b_path}' ")
             if diff.a_path == file_path or diff.b_path == file_path:
-                #print("Found match")
+                # print("Found match")
                 patch = diff.diff
-                patch = patch.decode('utf-8')
+                patch = patch.decode("utf-8")
                 break
         return patch
-    
+
     def get_file_contents(self, file_path, commit_id="HEAD"):
         logging.debug(f"Getting file contents for {file_path} in {commit_id}")
         commit = self.repo.commit(commit_id)
@@ -35,16 +38,23 @@ class GitDiff:
         blob = tree[file_path]
         logging.debug(f"Blob: {blob}")
         return blob.data_stream.read().decode()
-    
+
+    def get_file_contents_from_branch(self, file_path, branch="main"):
+        commit_id = self.get_commit_from_branch(branch)
+        return self.get_file_contents(file_path, commit_id)
+
     def get_commits_for_file(self, file_path, max_count=10):
-        commits_for_file_generator = self.repo.iter_commits(all=True, max_count=max_count, paths=file_path)
+        commits_for_file_generator = self.repo.iter_commits(
+            all=True, max_count=max_count, paths=file_path
+        )
         commits_for_file = list(commits_for_file_generator)
         return commits_for_file
-    
+
     def get_commit_from_branch(self, branch_name):
         return self.repo.heads[branch_name].commit
-    
+
     def get_branches(self):
         return self.repo.heads
-    
 
+    def checkout_branch(self, branch_name):
+        self.repo.git.checkout(branch_name)
