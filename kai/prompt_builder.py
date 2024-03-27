3179885bb4b6cac0155e5bd59150af434fb125c5
JonahSussman <sussmanjonah@gmail.com>
2024-03-27 12:59:34 -0400

    Added comments and updated errors to be more descriptive
    Signed-off-by: JonahSussman <sussmanjonah@gmail.com>

--- kai/prompt_builder.py
+++ kai/prompt_builder.py
@@ -5,6 +5,8 @@ from os import path
 
 import yaml
 
+from kai.kai_logging import KAI_LOG
+
 T_DIR = path.join(path.dirname(__file__), "data/templates")
 
 
@@ -59,6 +61,7 @@ class Section:
             self.file_path = "/dev/null"
             self.template = ""
 
+        # NOTE: Should this go in pb_build instead?
         if build_steps is None:
             build_steps = [{"format": self.uuid}]
 
@@ -124,6 +127,23 @@ class Env:
 
 
 def pb_concat(args: list, pb_env: Env, pb_vars: dict) -> str:
+    """
+    Description:
+        concat: [ arg... ] -> result
+
+        Takes a list of arguments, evaluates them and concatenates the results.
+        Returns the concatenation. If the arguments are not strings, throws an
+        error.
+
+    Arguments:
+        arg (str optional) - The string to concat
+
+    Returns:
+        result (str) - The concatenated string
+
+    Errors:
+        If any error is encountered, throws a PBError
+    """
     result = ""
 
     for arg in args:
@@ -135,16 +155,37 @@ def pb_concat(args: list, pb_env: Env, pb_vars: dict) -> str:
     return result
 
 
-def pb_format(args: list, pb_env: Env, pb_vars: dict) -> str | None:
-    # print(f"pb_format: {args=}")
+def pb_format(args: list, pb_env: Env, pb_vars: dict) -> str:
+    """
+    Description:
+        format: symbol pb_var_key -> result
+
+        Takes the section specified by symbol and formats it using Python's
+        .format and pb_vars as the argument. If pb_var_key is specified, then it
+        looks up that value in pb_vars, and uses that dict instead. If the value
+        for pb_var_key is list[dict] instead, format will run multiple times for
+        each entry in the list and concatenate the results.
+
+    Arguments:
+        symbol (str optional) - The string to look up in pb_vars
+        pb_var_key (str optional) - The key in pb_vars to use, if wanted
+
+    Returns:
+        result (str) - The formatted string
+
+    Errors:
+        If any error is encountered, throws a PBError
+    """
+
+    KAI_LOG.debug(f"pb_format: {args=}")
     if len(args) > 2:
-        raise PBError("Max 2 arguments.")
+        raise PBError(f"pb_format: Max 2 arguments. Got {len(args)}")
     if len(args) == 0:
         return ""
 
     section = pb_eval(args[0], pb_env, pb_vars)
     if not isinstance(section, Section):
-        raise PBError("Not a section!")
+        raise PBError(f"pb_format: First argument not a Section. Got {type(section)}")
 
     if len(args) == 1:
         return section.format_with_raise(pb_vars)
@@ -153,7 +194,14 @@ def pb_format(args: list, pb_env: Env, pb_vars: dict) -> str | None:
     if isinstance(pb_vars, dict):
         return section.format_with_raise(pb_vars)
     if not isinstance(pb_vars, list):
-        raise PBError("Uh-oh, not a list!")
+        raise PBError(
+            f"pb_format: If using with pb_var_key, pb_vars[pb_var_key] must be a list. Got {type(pb_vars)}"
+        )
+    for x in pb_vars:
+        if not isinstance(x, dict):
+            raise PBError(
+                f"pb_format: If using with pb_var_key, pb_vars[pb_var_key] each element must be a list. Got {type(x)}"
+            )
 
     result = ""
     for vars in pb_vars:
@@ -164,15 +212,37 @@ def pb_format(args: list, pb_env: Env, pb_vars: dict) -> str | None:
 
 
 def pb_build(args: list, pb_env: Env, pb_vars: dict):
+    """
+    Description:
+        build: symbol pb_var_key -> result
+
+        Takes the section specified by symbol and builds it using the section's
+        build_steps. (Note: Section has a build step of simply formatting the
+        string). If pb_var_key is specified, then it looks up that value in
+        pb_vars, and uses that dict instead. If the value for pb_var_key is
+        list[dict] instead, format will run multiple times for each entry in the
+        list and concatenate the results.
+
+    Arguments:
+        symbol (str optional) - The string to look up in pb_vars pb_var_key (str
+        optional) - The key in pb_vars to use, if wanted
+
+    Returns:
+        result (str) - The formatted string
+
+    Errors:
+        If any error is encountered, throws a PBError
+    """
+
     # print(f"pb_format: {args=}")
     if len(args) > 2:
-        raise PBError("Max 2 arguments.")
+        raise PBError(f"pb_build: Max 2 arguments. Got {len(args)}")
     if len(args) == 0:
         return ""
 
     section = pb_eval(args[0], pb_env, pb_vars)
     if not isinstance(section, Section):
-        raise PBError("Not a section!")
+        raise PBError(f"pb_build: First argument not a Section. Got {type(section)}")
 
     if len(args) == 1:
         for s in section.build_steps:
@@ -185,7 +255,14 @@ def pb_build(args: list, pb_env: Env, pb_vars: dict):
             r = pb_eval(s, pb_env, pb_vars)
         return r
     if not isinstance(pb_vars, list):
-        raise PBError("Uh-oh, not a list!")
+        raise PBError(
+            f"pb_build: If using with pb_var_key, pb_vars[pb_var_key] must be a list. Got {type(pb_vars)}"
+        )
+    for x in pb_vars:
+        if not isinstance(x, dict):
+            raise PBError(
+                f"pb_build: If using with pb_var_key, pb_vars[pb_var_key] each element must be a list. Got {type(x)}"
+            )
 
     result = ""
     for vars in pb_vars:
@@ -197,16 +274,41 @@ def pb_build(args: list, pb_env: Env, pb_vars: dict):
 
 
 def pb_one_of(args: list, pb_env: Env, pb_vars: dict) -> str:
+    """
+    Description:
+        one_of: [ arg... ] -> result
+
+        Goes through each argument sequentially and returns the argument that
+        does not error. Raises an error if the argument is not a string (for
+        now). Returns the first argument that doesn't have an error.
+
+    Arguments:
+        arg (str) - Each argument to evaluate
+
+    Returns:
+        result (str) - The first argument that didn't error or returned a string
+        when evaluated
+
+    Errors:
+        If any error is encountered, throws a PBError
+    """
+
     errors = []
     for arg in args:
         try:
             arg = pb_eval(arg, pb_env, pb_vars)
+            # NOTE: Maybe we could expand this to more than just strings
             if not (isinstance(arg, str) or arg is None):
-                raise PBError(f"Can only one_of strings or None. Got {type(arg)}.")
+                raise PBError(f"one_of: Only supports str or None. Got {type(arg)}.")
             if arg is not None:
                 return arg
         except Exception as e:
             errors.append(e)
+
+    if len(errors) == 0:
+        raise PBError(
+            "one_of: None of the arguments had an error, but also didn't succeed?"
+        )
 
     msg = "one_of: All of the arguments were invalid. Got the following errors:\n"
     for e in errors:
@@ -295,103 +397,66 @@ def build_prompt(section_name: str, pb_vars: dict):
 
 
 if __name__ == "__main__":
-    """
-    # pb = PromptBuilder(CONFIG_IBM_GRANITE)
 
-    # print(pb.build_prompt())
-    # input()
+    pb_vars = {
+        "src_file_name": "/var/src_file_name.java",
+        "src_file_contents": "import nothing;",
+        "analysis_line_number": 10,
+        "analysis_message": "ya done goofed!",
+    }
 
-    # pb.vars = {
-    #     "src_file_name": "/var/src_file_name.java",
-    #     "src_file_contents": "import nothing;",
-    #     "analysis_line_number": 10,
-    #     "analysis_message": "ya done goofed!",
-    # }
+    print(build_prompt(CONFIG_IBM_GRANITE, pb_vars))
 
-    # print(pb.build_prompt())
-    # input()
+    pb_vars = {
+        "src_file_name": "/var/src_file_name.java",
+        "solved_example_diff": "Here's a diff for ya!",
+        "solved_example_file_name": "the/solved/example.java",
+        "src_file_contents": "import nothing;",
+        "analysis_line_number": 10,
+        "analysis_message": "ya done goofed!",
+    }
 
-    # pb.vars = {
-    #     "src_file_name": "/var/src_file_name.java",
-    #     "solved_example_diff": "Here's a diff for ya!",
-    #     "solved_example_file_name": "the/solved/example.java",
-    #     "src_file_contents": "import nothing;",
-    #     "analysis_line_number": 10,
-    #     "analysis_message": "ya done goofed!",
-    # }
+    print(build_prompt(CONFIG_IBM_GRANITE, pb_vars))
 
-    # print(pb.build_prompt())
-    # input()
+    pb_vars = {
+        "src_file_name": "/var/src_file_name.java",
+        "solved_example_before": "File before!",
+        "solved_example_after": "File after!",
+        "solved_example_file_name": "the/solved/example.java",
+        "src_file_contents": "import nothing;",
+        "analysis_line_number": 10,
+        "analysis_message": "ya done goofed!",
+    }
 
-    # pb.vars = {
-    #     "src_file_name": "/var/src_file_name.java",
-    #     "solved_example_before": "File before!",
-    #     "solved_example_after": "File after!",
-    #     "solved_example_file_name": "the/solved/example.java",
-    #     "src_file_contents": "import nothing;",
-    #     "analysis_line_number": 10,
-    #     "analysis_message": "ya done goofed!",
-    # }
-
-    # print(pb.build_prompt())
-
-    """
+    print(build_prompt(CONFIG_IBM_GRANITE, pb_vars))
 
     # FIXME: Make pb_vars an environment, not a dict (cascading stuff)
 
-    build_prompt(
-        CONFIG_IBM_GRANITE_MF,
-        {
-            "src_file_name": "/var/src_file_name.java",
-            "src_file_language": "java",
-            "src_file_contents": "import nothing;",
-            "solved_example_file_name": "the/solved/example.java",
-            "incidents": [
-                {
-                    "issue_number": 1,
-                    "analysis_line_number": 10,
-                    "analysis_message": "ya done goofed!",
-                    "solved_example_before": "File before!",
-                    "solved_example_after": "File after!",
-                    "src_file_language": "java",
-                }
-            ],
-        },
-    )
-
-    print(
-        build_prompt(
-            CONFIG_IBM_LLAMA,
+    pb_vars = {
+        "src_file_name": "/var/src_file_name.java",
+        "src_file_language": "java",
+        "src_file_contents": "import nothing;",
+        "solved_example_file_name": "the/solved/example.java",
+        "incidents": [
             {
-                "analysis_message": "test message",
-                "src_file_name": "/var/src_file_name.java",
-                "src_file_language": "java",
-                "src_file_contents": "import nothing;",
+                "issue_number": 1,
                 "analysis_line_number": 10,
-            },
-        )
-    )
+                "analysis_message": "ya done goofed!",
+                "solved_example_before": "File before!",
+                "solved_example_after": "File after!",
+                "src_file_language": "java",
+            }
+        ],
+    }
 
-    exit()
+    print(build_prompt(CONFIG_IBM_GRANITE_MF, pb_vars))
 
-#     x = build_prompt(
-#         src_file_name="/var/whatever.java",
-#         src_file_contents="""import whatever;
+    pb_vars = {
+        "analysis_message": "test message",
+        "src_file_name": "/var/src_file_name.java",
+        "src_file_language": "java",
+        "src_file_contents": "import nothing;",
+        "analysis_line_number": 10,
+    }
 
-# class Java {
-
-# }
-# """,
-#         incidents=[
-#             {
-#                 "analysis_line_number": 10,
-#                 "analysis_message": "ya done goofed!",
-#             },
-#             {
-#                 "analysis_line_number": 10,
-#                 "analysis_message": "ya done goofed!",
-#             },
-#         ],
-#     )
-
-#     print(x, end="")
+    print(build_prompt(CONFIG_IBM_LLAMA, pb_vars))
