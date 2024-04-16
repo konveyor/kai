e6195f49fc7ff12b051a95c70c08fd25e4119b44
John Matthews <jwmatthews@gmail.com>
2024-04-16 15:06:48 -0400

    Script to help with Mac + podman 5

--- samples/macos/restart_podman_5_machine.sh
+++ samples/macos/restart_podman_5_machine.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-# Intended to be run with podman 4.x, don't run for podman 5.x
+# This script is intended for podman 5.x or higher
+# 2 changes:
+# 1) Need to bump the disk size to more than 100GB
+# 2) Remove the volume mounts, podman default behavior includes these volumes mounts, when we include them behavior breaks and Kantra cannot run
 #
 # Default variables can be overriden from environment
 : ${VM_NAME="kantra"}
 : ${MEM=8192}
 : ${CPUS=4}
-: ${DISK_SIZE=100}
+
+# Podman 5 is complaining when we attempt to us 100, it wants a disk bigger than 100
+: ${DISK_SIZE=105}
+
+
 
 # See https://github.com/konveyor/kantra/issues/91
 # See https://github.com/containers/podman/issues/16106#issuecomment-1317188581
@@ -14,7 +21,10 @@
 ulimit -n unlimited
 podman machine stop $VM_NAME
 podman machine rm $VM_NAME -f
-podman machine init $VM_NAME -v $HOME:$HOME -v /private/tmp:/private/tmp -v /var/folders/:/var/folders/
+
+# For podman 5.x we do not want to specify the volume mounts, they are enabled by default
+#podman machine init $VM_NAME -v $HOME:$HOME -v /private/tmp:/private/tmp -v /var/folders/:/var/folders/
+podman machine init $VM_NAME
 podman machine set $VM_NAME --cpus $CPUS --memory $MEM --disk-size $DISK_SIZE
 podman system connection default $VM_NAME
 podman machine start $VM_NAME
