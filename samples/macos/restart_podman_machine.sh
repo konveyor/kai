d50445a0e3d45483a76f575b4eea70a159f3715f
John Matthews <jwmatthews@gmail.com>
2024-04-30 10:43:07 -0400

    Update for podman5 on Mac (#177)

--- samples/macos/restart_podman_machine.sh
+++ samples/macos/restart_podman_machine.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+full_version=$(podman -v | awk '{print $3}')
+podman_major_version=$(echo "${full_version}" | cut -d '.' -f1)
+
 # Default variables can be overriden from environment
 : ${VM_NAME="kantra"}
 : ${MEM=8192}
 : ${CPUS=4}
-: ${DISK_SIZE=100}
+# Podman 5 will NOT run with '100' GB, it tells us it needs more than 100GB
+# "Error: new disk size must be larger than 100 GB"
+: ${DISK_SIZE=101}
 
 # See https://github.com/konveyor/kantra/issues/91
 # See https://github.com/containers/podman/issues/16106#issuecomment-1317188581
@@ -12,8 +17,20 @@
 ulimit -n unlimited
 podman machine stop $VM_NAME
 podman machine rm $VM_NAME -f
-podman machine init $VM_NAME -v $HOME:$HOME -v /private/tmp:/private/tmp -v /var/folders/:/var/folders/
-podman machine set $VM_NAME --cpus $CPUS --memory $MEM --disk-size $DISK_SIZE
+
+if [ "${podman_major_version}" -eq 4 ]; then
+	podman machine init $VM_NAME -v $HOME:$HOME -v /private/tmp:/private/tmp -v /var/folders/:/var/folders/
+else
+	# for podman 5.x we do not want to add the volume mounts, they are included by default and if we specify them we have odd behavior
+	podman machine init "${VM_NAME}"
+fi
+
+if ! podman machine set $VM_NAME --cpus $CPUS --memory $MEM --disk-size $DISK_SIZE; then
+	# We've had several subtle issues of not being able to set the memory and analysis later locking up
+	# So adding an explicit error check
+	exit 1
+fi
+
 podman system connection default $VM_NAME
 podman machine start $VM_NAME
 # Workaround for setting file limits inside of the podman machine VM
