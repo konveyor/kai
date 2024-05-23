9fe3d15b50213e10552bfaa18f4708264ea0cdf5
Jason Montleon <jmontleo@redhat.com>
2024-05-23 13:11:19 -0400

    Trunk and other fixes (#187)
    * Fix dependency issues
    
    Signed-off-by: Jason Montleon <jmontleo@redhat.com>
    
    * add recommend quotes and braces
    
    Signed-off-by: Jason Montleon <jmontleo@redhat.com>
    
    * Fix script shebang
    
    * Fix multidict GCC 14 (Fedora 40) build failure
    
    * Remove outdated notes file
    
    ---------
    
    Signed-off-by: Jason Montleon <jmontleo@redhat.com>

--- samples/macos/restart_podman_machine.sh
+++ samples/macos/restart_podman_machine.sh
@@ -15,24 +15,24 @@ podman_major_version=$(echo "${full_version}" | cut -d '.' -f1)
 # See https://github.com/containers/podman/issues/16106#issuecomment-1317188581
 # Setting file limits to unlimited on the Mac Host
 ulimit -n unlimited
-podman machine stop $VM_NAME
-podman machine rm $VM_NAME -f
+podman machine stop "${VM_NAME}"
+podman machine rm "${VM_NAME}" -f
 
 if [ "${podman_major_version}" -eq 4 ]; then
-	podman machine init $VM_NAME -v $HOME:$HOME -v /private/tmp:/private/tmp -v /var/folders/:/var/folders/
+	podman machine init "${VM_NAME}" -v "${HOME}":"${HOME}" -v /private/tmp:/private/tmp -v /var/folders/:/var/folders/
 else
 	# for podman 5.x we do not want to add the volume mounts, they are included by default and if we specify them we have odd behavior
 	podman machine init "${VM_NAME}"
 fi
 
-if ! podman machine set $VM_NAME --cpus $CPUS --memory $MEM --disk-size $DISK_SIZE; then
+if ! podman machine set "${VM_NAME}" --cpus "${CPUS}" --memory "${MEM}" --disk-size "${DISK_SIZE}"; then
 	# We've had several subtle issues of not being able to set the memory and analysis later locking up
 	# So adding an explicit error check
 	exit 1
 fi
 
-podman system connection default $VM_NAME
-podman machine start $VM_NAME
+podman system connection default "${VM_NAME}"
+podman machine start "${VM_NAME}"
 # Workaround for setting file limits inside of the podman machine VM
 # https://github.com/konveyor/kantra/issues/111
 podman machine ssh kantra "echo *      soft      nofile      65535 | sudo tee -a /etc/security/limits.conf"
