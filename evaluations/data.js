f5a55486996bbed85ef40ce1e33d3b09c29897ec
github-action-benchmark <github@users.noreply.github.com>
2024-06-25 15:27:14 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for ebef75bbc982400bf7ddd5abd2c769c05164ed00

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719329233212,
+  "lastUpdate": 1719329234083,
   "repoUrl": "https://github.com/konveyor-ecosystem/kai",
   "entries": {
     "Benchmark": [
@@ -28,6 +28,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "ChatIBMGenAI_codellama_codellama-34b-instruct_example_a",
             "value": "79.0",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jmontleo@redhat.com",
+            "name": "Jason Montleon",
+            "username": "jmontleon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ebef75bbc982400bf7ddd5abd2c769c05164ed00",
+          "message": "Add Evaluation GH Action (#209)\n\nSigned-off-by: Jason Montleon <jmontleo@redhat.com>",
+          "timestamp": "2024-06-25T11:23:42-04:00",
+          "tree_id": "3ed5b4d09f05cff5ba4b6c16a8820d4f75c6fbe2",
+          "url": "https://github.com/konveyor-ecosystem/kai/commit/ebef75bbc982400bf7ddd5abd2c769c05164ed00"
+        },
+        "date": 1719329233461,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_mistralai_mixtral-8x7b-instruct-v01_example_a",
+            "value": "295.0",
             "unit": "Match"
           }
         ]
