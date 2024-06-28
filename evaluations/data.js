96270645f2c8775b4662b927d3d84ae6ab522b16
github-action-benchmark <github@users.noreply.github.com>
2024-06-28 15:16:44 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for a7f85f8478e3678568786786a05889388fbb5611

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719587788125,
+  "lastUpdate": 1719587804623,
   "repoUrl": "https://github.com/konveyor-ecosystem/kai",
   "entries": {
     "Benchmark": [
@@ -289,6 +289,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "ChatIBMGenAI_codellama_codellama-34b-instruct_example_a",
             "value": "79.0",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jwmatthews@gmail.com",
+            "name": "John Matthews",
+            "username": "jwmatthews"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a7f85f8478e3678568786786a05889388fbb5611",
+          "message": "Update README, remove mention of mock-client.py, tweak to demo and debug (#201)\n\ninfo\r\n\r\nSigned-off-by: John Matthews <jwmatthews@gmail.com>",
+          "timestamp": "2024-06-28T11:13:15-04:00",
+          "tree_id": "a7b79ff3f6dbbe6d28755ad9f4276d6dfabea939",
+          "url": "https://github.com/konveyor-ecosystem/kai/commit/a7f85f8478e3678568786786a05889388fbb5611"
+        },
+        "date": 1719587803481,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_mistralai_mixtral-8x7b-instruct-v01_example_a",
+            "value": "295.0",
             "unit": "Match"
           }
         ]
