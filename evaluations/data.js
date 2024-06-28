cded034bcb50bb9ef6d50dca402fe7f7cc5151ca
github-action-benchmark <github@users.noreply.github.com>
2024-06-28 15:16:28 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for cb2b38c47464df71bc6c38eb7d7d51dca31d5731

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719587784762,
+  "lastUpdate": 1719587788125,
   "repoUrl": "https://github.com/konveyor-ecosystem/kai",
   "entries": {
     "Benchmark": [
@@ -260,6 +260,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "ChatIBMGenAI_deepseek-ai_deepseek-coder-33b-instruct_example_a",
             "value": "1.0",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "42743659+JonahSussman@users.noreply.github.com",
+            "name": "Jonah Sussman",
+            "username": "JonahSussman"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cb2b38c47464df71bc6c38eb7d7d51dca31d5731",
+          "message": "Updated requirements.txt (#216)\n\nSigned-off-by: JonahSussman <sussmanjonah@gmail.com>",
+          "timestamp": "2024-06-28T11:12:51-04:00",
+          "tree_id": "27284a8defc2e45bcdf640e68d6dc91935f1dcf2",
+          "url": "https://github.com/konveyor-ecosystem/kai/commit/cb2b38c47464df71bc6c38eb7d7d51dca31d5731"
+        },
+        "date": 1719587786927,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_codellama_codellama-34b-instruct_example_a",
+            "value": "79.0",
             "unit": "Match"
           }
         ]
