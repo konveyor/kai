6b83603ed28fa17c485aca7101c9c4503a4d5fcb
github-action-benchmark <github@users.noreply.github.com>
2024-07-02 01:54:33 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 5859db0c30a6fcbefdb85fadf3187647308f0f97

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719885267234,
+  "lastUpdate": 1719885273909,
   "repoUrl": "https://github.com/konveyor-ecosystem/kai",
   "entries": {
     "Benchmark": [
@@ -1071,6 +1071,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_deepseek-ai_deepseek-coder-33b-instruct_example_a",
+            "value": "1.0",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jmontleo@redhat.com",
+            "name": "Jason Montleon",
+            "username": "jmontleon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5859db0c30a6fcbefdb85fadf3187647308f0f97",
+          "message": ":sparkles: Add GH Action to build the image (#223)\n\nSigned-off-by: Jason Montleon <jmontleo@redhat.com>",
+          "timestamp": "2024-07-01T21:50:27-04:00",
+          "tree_id": "ee720dc18a8be139c8152ba4aa7119eeb9d4dcff",
+          "url": "https://github.com/konveyor-ecosystem/kai/commit/5859db0c30a6fcbefdb85fadf3187647308f0f97"
+        },
+        "date": 1719885273368,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_meta-llama_llama-3-70b-instruct_example_a",
             "value": "1.0",
             "unit": "Match"
           }
