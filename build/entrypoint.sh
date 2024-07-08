413dbcd30b89d7f285d846e25fb12c0851165d53
Jason Montleon <jmontleo@redhat.com>
2024-07-08 12:44:42 -0400

    entrypoint.sh container script improvements (#225)
    * :sparkles: entrypoint.sh container script improvements
    
    Signed-off-by: Jason Montleon <jmontleo@redhat.com>
    
    * Add hub importer support
    
    Signed-off-by: Jason Montleon <jmontleo@redhat.com>
    
    ---------
    
    Signed-off-by: Jason Montleon <jmontleo@redhat.com>

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
-# If we are using compose.yml use the repo config.toml
-if [ -f /podman_compose/kai/config.toml ]; then
-  cp /podman_compose/kai/config.toml /kai/kai/config.toml
-  sed -i 's/^host\ =.*/host = "kai_db"/g' /kai/kai/config.toml
-  sed -i "s/^database =.*/database = \"$POSTGRES_DB\"/g" /kai/kai/config.toml
-  sed -i "s/^user =.*/user = \"$POSTGRES_USER\"/g" /kai/kai/config.toml
-  sed -i "s/^password =.*/password =\"$POSTGRES_PASSWORD\"/g" /kai/kai/config.toml
+# If we are using podman or docker compose use the repo config.toml
+if [[ -f /podman_compose/kai/config.toml ]]; then
+	cp /podman_compose/kai/config.toml /kai/kai/config.toml
+	sed -i 's/^host\ =.*/host = "kai_db"/g' /kai/kai/config.toml
+	sed -i "s/^database =.*/database = \"${POSTGRES_DB}\"/g" /kai/kai/config.toml
+	sed -i "s/^user =.*/user = \"${POSTGRES_USER}\"/g" /kai/kai/config.toml
+	sed -i "s/^password =.*/password =\"${POSTGRES_PASSWORD}\"/g" /kai/kai/config.toml
 fi
 
-TABLE=applications
-SQL_EXISTS=$(printf '\dt "%s"' "$TABLE")
-if [[ $(PGPASSWORD="$POSTGRES_PASSWORD" psql -h "kai_db" -U $POSTGRES_USER -d $POSTGRES_DB -c "$SQL_EXISTS") ]]
-then
-    echo "load-data has run already"
+until PGPASSWORD="${POSTGRES_PASSWORD}" pg_isready -q -h kai_db -U "${POSTGRES_USER}" -d "${POSTGRES_DB}"; do
+	sleep 1
+done
+
+if [[ ${MODE} != "importer" ]]; then
+	if [[ ${USE_HUB_IMPORTER} == "False" ]]; then
+		TABLE=applications
+		SQL_EXISTS=$(printf "\dt %s" "${TABLE}")
+		STDERR="Did not find any relation"
+		# trunk-ignore(shellcheck/SC2312)
+		if PGPASSWORD="${POSTGRES_PASSWORD}" psql -h kai_db -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" -c "${SQL_EXISTS}" 2>&1 | grep -q -v "${STDERR}"; then
+			echo "################################################"
+			echo "load-data has run already run, starting server.#"
+			echo "################################################"
+		else
+			echo "################################################"
+			echo "load-data has never been run.                  #"
+			echo "Please wait, this will take a few minutes.     #"
+			echo "################################################"
+			sleep 5
+			cd /kai/samples || exit
+			./fetch_apps.py
+			cd /kai || exit
+			python ./kai/service/incident_store/psql.py --config_filepath ./kai/config.toml --drop_tables False
+			echo "################################################"
+			echo "load-data has completed, starting server.      #"
+			echo "################################################"
+			sleep 5
+		fi
+	fi
+	PYTHONPATH="/kai/kai" exec gunicorn --timeout 3600 -w "${NUM_WORKERS}" --bind 0.0.0.0:8080 --worker-class aiohttp.GunicornWebWorker 'kai.server:app()'
 else
-    echo "load-data has not run yet, starting ..."
-    echo "Fetching examples"
-    cd /kai/samples
-    ./fetch_apps.py
-    cd /kai
-    python ./kai/service/incident_store/psql.py  --config_filepath ./kai/config.toml --drop_tables False
+	cd /kai || exit
+	python ./kai/hub_importer.py --loglevel "${LOGLEVEL}" --config_filepath ./kai/config.toml "${IMPORTER_ARGS}" "${HUB_URL}"
 fi
-
-PYTHONPATH="/kai/kai" exec gunicorn --timeout 3600 -w $NUM_WORKERS --bind 0.0.0.0:8080 --worker-class aiohttp.GunicornWebWorker 'kai.server:app()'
