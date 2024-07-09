7d8395744c2d49bd7a180266fca2e7f29d7c7b6e
github-action-benchmark <github@users.noreply.github.com>
2024-07-09 19:53:31 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for e29049c282cf7102e3f7e50357f6fab968500e8c

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720554803286,
+  "lastUpdate": 1720554811656,
   "repoUrl": "https://github.com/konveyor-ecosystem/kai",
   "entries": {
     "Benchmark": [
@@ -1768,6 +1768,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "ChatIBMGenAI_codellama_codellama-34b-instruct_example_a",
             "value": "1.0",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jmontleo@redhat.com",
+            "name": "Jason Montleon",
+            "username": "jmontleon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e29049c282cf7102e3f7e50357f6fab968500e8c",
+          "message": ":sparkles: Container and Compose Improvements (#231)\n\nSigned-off-by: Jason Montleon <jmontleo@redhat.com>",
+          "timestamp": "2024-07-09T15:49:57-04:00",
+          "tree_id": "cdf36136c05d4c55764922305bbd0233fb8720f8",
+          "url": "https://github.com/konveyor-ecosystem/kai/commit/e29049c282cf7102e3f7e50357f6fab968500e8c"
+        },
+        "date": 1720554810901,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_mistralai_mixtral-8x7b-instruct-v01_example_a",
+            "value": "295.0",
             "unit": "Match"
           }
         ]
