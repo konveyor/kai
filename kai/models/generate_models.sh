2c89c9aabc2e77b1aedda197dc97d44845a95fbe
JonahSussman <sussmanjonah@gmail.com>
2024-07-15 16:29:07 -0400

    Renamed to `report_types`
    Signed-off-by: JonahSussman <sussmanjonah@gmail.com>

--- kai/models/generate_models.sh
+++ kai/models/generate_models.sh
@@ -2,5 +2,5 @@
 
 # for each .yaml file in the directory, generate a .py file with the same name
 for f in *.yaml; do
-	datamodel-codegen --input "${f}" --input-file-type jsonschema --output "${f%.*}".py
+	datamodel-codegen --input "${f}" --input-file-type jsonschema --output-model-type pydantic_v2.BaseModel --output "${f%.*}".py
 done
