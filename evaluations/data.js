fa04929805e6de0b8a92f5ff7fc6ed6ab56a5bc1
github-action-benchmark <github@users.noreply.github.com>
2024-07-22 16:45:55 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 71e8fe931e634ed334e3abe5a2a8e12f68ed6f40

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721666657940,
+  "lastUpdate": 1721666755454,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -2579,6 +2579,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_mistralai_mistral-7b-v0-1_example_a",
+            "value": "",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jwmatthews@gmail.com",
+            "name": "John Matthews",
+            "username": "jwmatthews"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "71e8fe931e634ed334e3abe5a2a8e12f68ed6f40",
+          "message": "Build demo.* tags/branches and push to quay (#243)\n\nSigned-off-by: John Matthews <jwmatthews@gmail.com>",
+          "timestamp": "2024-07-22T12:40:22-04:00",
+          "tree_id": "e0bd2eac60729b9dc145d1aa4bc1f59dbf698ddc",
+          "url": "https://github.com/konveyor/kai/commit/71e8fe931e634ed334e3abe5a2a8e12f68ed6f40"
+        },
+        "date": 1721666753923,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_meta-llama_llama-3-70b-instruct_example_a",
             "value": "",
             "unit": "Match"
           }
