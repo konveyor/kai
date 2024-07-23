59b90ae354d114453e184cb46316754938c71c20
github-action-benchmark <github@users.noreply.github.com>
2024-07-23 14:22:24 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 0ba9e33fecc4ece06f851107f17a55d95d6a6b37

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721744522864,
+  "lastUpdate": 1721744544949,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -2695,6 +2695,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_meta-llama_llama-3-70b-instruct_example_a",
+            "value": "",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jwmatthews@gmail.com",
+            "name": "John Matthews",
+            "username": "jwmatthews"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0ba9e33fecc4ece06f851107f17a55d95d6a6b37",
+          "message": "Fix writing trace data to local disk with podman compose up (#245)\n\nSigned-off-by: John Matthews <jwmatthews@gmail.com>",
+          "timestamp": "2024-07-23T09:56:10-04:00",
+          "tree_id": "963df96d98edd84866642df381d3d457d26d83e6",
+          "url": "https://github.com/konveyor/kai/commit/0ba9e33fecc4ece06f851107f17a55d95d6a6b37"
+        },
+        "date": 1721744544399,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_codellama_codellama-34b-instruct_example_a",
             "value": "",
             "unit": "Match"
           }
