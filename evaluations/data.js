473b53b3c8f662881263ccfdbf4ce1e4e88e5b0d
github-action-benchmark <github@users.noreply.github.com>
2024-07-25 21:00:53 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 29f6e2bf69a32e50314655549f33086026ed3536

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721941252756,
+  "lastUpdate": 1721941253736,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -3507,6 +3507,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_deepseek-ai_deepseek-coder-33b-instruct_example_a",
+            "value": "",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jwmatthews@gmail.com",
+            "name": "John Matthews",
+            "username": "jwmatthews"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "29f6e2bf69a32e50314655549f33086026ed3536",
+          "message": ":book: Reworking docs to breakout main README.md (#254)\n\nSigned-off-by: John Matthews <jwmatthews@gmail.com>",
+          "timestamp": "2024-07-25T16:57:48-04:00",
+          "tree_id": "0d29e57453520abedc3fbd7aab441d6abf75a20e",
+          "url": "https://github.com/konveyor/kai/commit/29f6e2bf69a32e50314655549f33086026ed3536"
+        },
+        "date": 1721941253170,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_mistralai_mixtral-8x7b-instruct-v01_example_a",
             "value": "",
             "unit": "Match"
           }
