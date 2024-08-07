75ce1812aac8d24399902e0ad2e014476a28823a
github-action-benchmark <github@users.noreply.github.com>
2024-08-07 16:24:49 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 1c5e827000304c6a460f30b53ba7818020253d6a

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723047888103,
+  "lastUpdate": 1723047889451,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -5566,6 +5566,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_deepseek-ai_deepseek-coder-33b-instruct_example_a",
+            "value": "1.0",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "107868772+brf153@users.noreply.github.com",
+            "name": "Devaansh Bhandari",
+            "username": "brf153"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1c5e827000304c6a460f30b53ba7818020253d6a",
+          "message": "fix: Fix Path Resolution in `process_log_dir_replacements` Function (#279)\n\n* fix process_log_dir_replacements function in kai_logging.py\r\n\r\nSigned-off-by: brf153 <153hsb@gmail.com>\r\n\r\n* correct kai_project_directory\r\n\r\nSigned-off-by: brf153 <153hsb@gmail.com>\r\n\r\n---------\r\n\r\nSigned-off-by: brf153 <153hsb@gmail.com>",
+          "timestamp": "2024-08-07T12:23:38-04:00",
+          "tree_id": "1ce3a59370fb409276d36ebe280280c123e82f7a",
+          "url": "https://github.com/konveyor/kai/commit/1c5e827000304c6a460f30b53ba7818020253d6a"
+        },
+        "date": 1723047887336,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_codellama_codellama-34b-instruct_example_a",
             "value": "1.0",
             "unit": "Match"
           }
