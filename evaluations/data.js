112282a0754d74fd308dce4fdfafd4db909d71b9
github-action-benchmark <github@users.noreply.github.com>
2024-08-09 14:50:03 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 63503d1f1b2fd6c738999d022359468752560313

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723215000497,
+  "lastUpdate": 1723215003261,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -6002,6 +6002,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "ChatIBMGenAI_mistralai_mixtral-8x7b-instruct-v01_example_a",
             "value": "295.0",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jwmatthews@gmail.com",
+            "name": "John Matthews",
+            "username": "jwmatthews"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "63503d1f1b2fd6c738999d022359468752560313",
+          "message": "📖 Update README.md for link to IDE usage (#286)\n\n* Update README.md for link to IDE usage\r\n\r\nSigned-off-by: John Matthews <jwmatthews@gmail.com>\r\n\r\n* Update README.md\r\n\r\nSigned-off-by: John Matthews <jwmatthews@gmail.com>\r\n\r\n* Update README.md\r\n\r\nSigned-off-by: John Matthews <jwmatthews@gmail.com>\r\n\r\n---------\r\n\r\nSigned-off-by: John Matthews <jwmatthews@gmail.com>",
+          "timestamp": "2024-08-09T10:48:51-04:00",
+          "tree_id": "c1628bafbe22148874fb3858d04cf5bf73c916db",
+          "url": "https://github.com/konveyor/kai/commit/63503d1f1b2fd6c738999d022359468752560313"
+        },
+        "date": 1723215002057,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_codellama_codellama-34b-instruct_example_a",
+            "value": "1.0",
             "unit": "Match"
           }
         ]
