574a72d0d31f54aadca1a03346ec69460f5da260
github-action-benchmark <github@users.noreply.github.com>
2024-08-19 21:50:18 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 498669a61fc355b3bb72bc65c01b480b4e9a1ec3

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724104216711,
+  "lastUpdate": 1724104218181,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -7307,6 +7307,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "ChatIBMGenAI_codellama_codellama-34b-instruct_example_a",
             "value": "79.0",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "maira.usman5703@gmail.com",
+            "name": "Maira Usman",
+            "username": "Myrausman"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "498669a61fc355b3bb72bc65c01b480b4e9a1ec3",
+          "message": "Fix GENAI_KEY issue to allow demo_mode without requiring API key (#292)\n\n* Fix GENAI_KEY for demo_mode\r\n\r\nSigned-off-by: Myrausman <maira.usman5703@gmail.com>\r\n\r\n* Use env vars if set in demo mode\r\n\r\nSigned-off-by: Myrausman <maira.usman5703@gmail.com>\r\n\r\n* Handle various 'true' cases\r\n\r\nSigned-off-by: Myrausman <maira.usman5703@gmail.com>\r\n\r\n* Fix api endpoint\r\n\r\nSigned-off-by: Myrausman <maira.usman5703@gmail.com>\r\n\r\n---------\r\n\r\nSigned-off-by: Myrausman <maira.usman5703@gmail.com>",
+          "timestamp": "2024-08-19T17:49:01-04:00",
+          "tree_id": "70e466f627d7ba7d67e71a3d82e7cfe405a48ef8",
+          "url": "https://github.com/konveyor/kai/commit/498669a61fc355b3bb72bc65c01b480b4e9a1ec3"
+        },
+        "date": 1724104215990,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_deepseek-ai_deepseek-coder-33b-instruct_example_a",
+            "value": "1.0",
             "unit": "Match"
           }
         ]
