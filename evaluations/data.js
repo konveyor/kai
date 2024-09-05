0f8edb8ff2cf8fd3737e89c4f28908bc316046a2
github-action-benchmark <github@users.noreply.github.com>
2024-09-05 12:52:35 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 0006e284533c72074284c9e39ce113a1da1a5f81

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725540754220,
+  "lastUpdate": 1725540755027,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -9017,6 +9017,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_meta-llama_llama-3-70b-instruct_example_a",
+            "value": "",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pgaikwad@redhat.com",
+            "name": "Pranav Gaikwad",
+            "username": "pranavgaikwad"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0006e284533c72074284c9e39ce113a1da1a5f81",
+          "message": ":bug: fix things for Windows (#351)\n\nSigned-off-by: Pranav Gaikwad <pgaikwad@redhat.com>",
+          "timestamp": "2024-09-05T08:51:22-04:00",
+          "tree_id": "e2fbef27816fa01cf473e11727cc0bc3af0aa605",
+          "url": "https://github.com/konveyor/kai/commit/0006e284533c72074284c9e39ce113a1da1a5f81"
+        },
+        "date": 1725540754412,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_mistralai_mistral-7b-v0-1_example_a",
             "value": "",
             "unit": "Match"
           }
