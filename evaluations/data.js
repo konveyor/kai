7cb2378e242ffc4f8351a85b0d1fe3c0d9e69fdf
github-action-benchmark <github@users.noreply.github.com>
2024-09-12 20:18:31 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for fa08eb412e5c003d58fb63ca0c9defebd8f9bae8

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726172310083,
+  "lastUpdate": 1726172311073,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -9829,6 +9829,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_mistralai_mixtral-8x7b-instruct-v01_example_a",
+            "value": "",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "42743659+JonahSussman@users.noreply.github.com",
+            "name": "Jonah Sussman",
+            "username": "JonahSussman"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fa08eb412e5c003d58fb63ca0c9defebd8f9bae8",
+          "message": ":bug: Fixed Amazon Bedrock dependency (#361)\n\n* Fixed aws dependency\r\n\r\nSigned-off-by: JonahSussman <sussmanjonah@gmail.com>\r\n\r\n* Fixed class name\r\n\r\nSigned-off-by: JonahSussman <sussmanjonah@gmail.com>\r\n\r\n---------\r\n\r\nSigned-off-by: JonahSussman <sussmanjonah@gmail.com>",
+          "timestamp": "2024-09-12T16:17:15-04:00",
+          "tree_id": "b91c6e24a56f3c577d7c783d10e39db66480a324",
+          "url": "https://github.com/konveyor/kai/commit/fa08eb412e5c003d58fb63ca0c9defebd8f9bae8"
+        },
+        "date": 1726172309472,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_mistralai_mistral-7b-v0-1_example_a",
             "value": "",
             "unit": "Match"
           }
