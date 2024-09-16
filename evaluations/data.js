3d81cab90ef785ecadfbd37f016e157efcdf7a3d
github-action-benchmark <github@users.noreply.github.com>
2024-09-16 19:54:22 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for bfa1288ab97f28ec6af87780c109171a0ecb1cc2

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726516460558,
+  "lastUpdate": 1726516462504,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -9945,6 +9945,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_mistralai_mixtral-8x7b-instruct-v01_example_a",
+            "value": "",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shawn@hurley.page",
+            "name": "Shawn Hurley",
+            "username": "shawn-hurley"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bfa1288ab97f28ec6af87780c109171a0ecb1cc2",
+          "message": ":sparkles: Adding testing notebook for compilation error agent (#364)\n\n* Adding testing notebook for compilation error agent\r\n\r\nSigned-off-by: Shawn Hurley <shawn@hurley.page>\r\n\r\n* adding check on output\r\n\r\nSigned-off-by: Shawn Hurley <shawn@hurley.page>\r\n\r\n---------\r\n\r\nSigned-off-by: Shawn Hurley <shawn@hurley.page>",
+          "timestamp": "2024-09-16T15:52:28-04:00",
+          "tree_id": "b8bd52f2eb0c0bec5dc6e834b4778a991e5abc5f",
+          "url": "https://github.com/konveyor/kai/commit/bfa1288ab97f28ec6af87780c109171a0ecb1cc2"
+        },
+        "date": 1726516461342,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_deepseek-ai_deepseek-coder-33b-instruct_example_a",
             "value": "",
             "unit": "Match"
           }
