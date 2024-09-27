65242f6960a9d81bd56682699e38309cd7dc9b6c
Pranav Gaikwad <pgaikwad@redhat.com>
2024-09-27 12:53:14 -0400

    :sparkles: Add reflection agent (#387)
    * :sparkles: add reflection agent
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>
    Signed-off-by: Shawn Hurley <shawn@hurley.page>

--- playpen/repo_level_awareness/agents/compiler_agent.py
+++ playpen/repo_level_awareness/agents/compiler_agent.py
@@ -1,27 +1,35 @@
-from playpen.repo_level_awareness.api import Task, TaskResult, Agent
-from playpen.repo_level_awareness.maven_validator import MavenCompilerError
-from langchain_core.language_models.chat_models import BaseChatModel
-from langchain_core.messages import HumanMessage, SystemMessage, BaseMessage
-from jinja2 import Template
-import asyncio
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from typing import List
+
+from jinja2 import Template
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
+
+from playpen.repo_level_awareness.api import Agent, Task, TaskResult
+from playpen.repo_level_awareness.git_vfs import RepoContextManager
+from playpen.repo_level_awareness.maven_validator import MavenCompilerError
+
 
 @dataclass
-class MavenCompilerLLMResponse():
+class MavenCompilerLLMResponse:
     reasoning: str
     java_file: str
     addional_information: str
+    file_path: str = ""
+    input_file: str = ""
+    input_errors: List[str] = field(factory_method=list)
 
 
 class MavenCompilerAgent(Agent):
     """This agent is reponsible for taking a set of maven compiler issues and solving.
-     
+
     For a given file it will asking LLM's for the changes that are needed for the at whole file
     returning the results.
     """
 
-    system_message=SystemMessage(content="""
+    system_message = SystemMessage(
+        content="""
     I will give you compiler errors and the offending line of code, and you will need to use the file to determine how to fix them. You should only use compiler errors to determine what to fix.
 
     Make sure that the references to any changed types are kept.
@@ -29,9 +37,11 @@ class MavenCompilerAgent(Agent):
     You must reason through the required changes and rewrite the Java file to make it compile. 
 
     You will then provide an step-by-step explaination of the changes required tso that someone could recreate it in a similar situation. 
-    """)
+    """
+    )
 
-    chat_message_template=Template("""
+    chat_message_template = Template(
+        """
     [INST]
     ## Compile Errors
     {{compile_errors}}
@@ -56,7 +66,7 @@ class MavenCompilerAgent(Agent):
 
     def __init__(self, llm: BaseChatModel) -> None:
         self.__llm = llm
-    
+
     def refine_task(self, errors: list[str]) -> None:
         """We currently do not refine the tasks"""
         return super().refine_task(errors)
@@ -69,35 +79,43 @@ class MavenCompilerAgent(Agent):
         """Will determine if the task if a MavenCompilerError, and if we can handle these issues."""
         print(task)
         return isinstance(task, MavenCompilerError)
-    
-    def execute_task(self, task: Task) -> TaskResult:
+
+    def execute_task(self, rcm: RepoContextManager, task: Task) -> TaskResult:
         """This will be responsible for getting the full file from LLM and updating the file on disk"""
 
         # convert the task to the MavenCompilerError
         if not isinstance(task, MavenCompilerError):
             return TaskResult(encountered_errors=[], modified_files=[])
 
-        with open(task.file) as f: src_file_contents= f.read()
+        with open(task.file) as f:
+            src_file_contents = f.read()
 
         line_of_code = src_file_contents.split("\n")[task.line]
 
         compile_errors = f"Line of code: {line_of_code};\n{task.message}"
 
         content = self.chat_message_template.render(
-            src_file_contents=src_file_contents,
-            compile_errors=compile_errors)
+            src_file_contents=src_file_contents, compile_errors=compile_errors
+        )
 
-        aimessage = self.__llm.invoke([self.system_message, HumanMessage(content=content)])
+        aimessage = self.__llm.invoke(
+            [self.system_message, HumanMessage(content=content)]
+        )
 
         resp = self.parse_llm_response(aimessage)
-
         print(resp)
+        resp.file_path = task.file
+        resp.input_file = src_file_contents
+        resp.input_errors = [task.message]
 
         # rewrite the file, based on the java file returned
-        with open(task.file, "w") as f: f.write(resp.java_file)
+        with open(task.file, "w") as f:
+            f.write(resp.java_file)
+
+        rcm.commit("compiler", resp)
 
         return TaskResult(modified_files=[Path(task.file)], encountered_errors=[])
-    
+
     def parse_llm_response(self, message: BaseMessage) -> MavenCompilerLLMResponse:
         """Private method that will be used to parse the contents and get the results"""
 
@@ -126,13 +144,13 @@ class MavenCompilerAgent(Agent):
             if in_java_file:
                 if "```java" in line or "```" in line:
                     continue
-                java_file= "\n".join([java_file, line])
+                java_file = "\n".join([java_file, line])
             if in_reasoning:
                 reasoning = "\n".join([reasoning, line])
             if in_additional_details:
                 additional_details = "\n".join([additional_details, line])
-        return MavenCompilerLLMResponse(reasoning=reasoning, java_file=java_file, addional_information=additional_details)
-
-
-
-    
+        return MavenCompilerLLMResponse(
+            reasoning=reasoning,
+            java_file=java_file,
+            addional_information=additional_details,
+        )
