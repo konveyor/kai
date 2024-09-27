05d9805dddf9d89ad2addc44cf4a7c6d758ad98e
Pranav Gaikwad <pgaikwad@redhat.com>
2024-09-27 12:53:14 -0400

    :sparkles: Add reflection agent (#387)
    * :sparkles: add reflection agent
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- playpen/repo_level_awareness/api.py
+++ playpen/repo_level_awareness/api.py
@@ -1,9 +1,8 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Generator, Iterator, Optional
 
-from pydantic import BaseModel
+from .git_vfs import RepoContextManager
 
 
 @dataclass
@@ -38,21 +37,26 @@ class ValidationStep(ABC):
     def __init__(self, RpcClientConfig: RpcClientConfig) -> None:
         self.config = RpcClientConfig
 
+    @abstractmethod
     def run(self) -> ValidationResult:
         pass
 
 
 class Agent(ABC):
+    @abstractmethod
     def can_handle_task(self, task: Task) -> bool:
         pass
 
-    def execute_task(self, task: Task) -> TaskResult:
+    @abstractmethod
+    def execute_task(self, rcm: RepoContextManager, task: Task) -> TaskResult:
         pass
 
+    @abstractmethod
     def refine_task(self, errors: list[str]) -> None:
         # Knows that it's the refine step so that it might not spawn as much
         # stuff.
         pass
 
+    @abstractmethod
     def can_handle_error(self, errors: list[str]) -> bool:
         pass
