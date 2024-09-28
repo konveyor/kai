2910357bb88f99d4931ce71ff3c6566be47a5751
github-action-benchmark <github@users.noreply.github.com>
2024-09-28 16:52:53 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 3743e0ad94de0be98e9fccdb004288c6883eb386

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727542372973,
+  "lastUpdate": 1727542373940,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -10989,6 +10989,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_deepseek-ai_deepseek-coder-33b-instruct_example_a",
+            "value": "",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jwmatthews@gmail.com",
+            "name": "John Matthews",
+            "username": "jwmatthews"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3743e0ad94de0be98e9fccdb004288c6883eb386",
+          "message": "Update cached responses for gpt-4o and gpt-4-turbo (#402)\n\nSigned-off-by: John Matthews <jwmatthews@gmail.com>",
+          "timestamp": "2024-09-28T11:51:37-05:00",
+          "tree_id": "e441d370b8aa67abfea34f95b71e89392298ea53",
+          "url": "https://github.com/konveyor/kai/commit/3743e0ad94de0be98e9fccdb004288c6883eb386"
+        },
+        "date": 1727542372662,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_mistralai_mixtral-8x7b-instruct-v01_example_a",
             "value": "",
             "unit": "Match"
           }
