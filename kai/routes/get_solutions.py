27450599aacff20d482442be5814225cc7bd5e0c
Pranav Gaikwad <pgaikwad@redhat.com>
2024-10-09 15:50:48 -0400

    add response model
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- kai/routes/get_solutions.py
+++ kai/routes/get_solutions.py
@@ -1,20 +1,25 @@
 import logging
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 from aiohttp import web
 from aiohttp.web_request import Request
 from pydantic import BaseModel
 
 from kai.routes.util import to_route
+from kai.service.incident_store.incident_store import Solution
 
 KAI_LOG = logging.getLogger(__name__)
 
 
-class PostGetSolvedExamplesParams(BaseModel):
+class PostGetSolutionsParams(BaseModel):
     ruleset_name: str
     violation_name: str
     incident_variables: Dict
     incident_snip: Optional[str] = None
+
+
+class ResponseGetSolutions(BaseModel):
+    solutions: List[Solution]
 
 
 class PostSubmitAcceptedSolution(BaseModel):
@@ -25,18 +30,16 @@ class PostSubmitAcceptedSolution(BaseModel):
 async def post_get_solutions(request: Request):
     KAI_LOG.debug(f"post_get_solutions recv'd: {request}")
 
-    params = PostGetSolvedExamplesParams.model_validate(await request.json())
+    params = PostGetSolutionsParams.model_validate(await request.json())
 
-    solutions = request.app[
+    solutions: List[Solution] = request.app[
         # TODO (pgaikwad): type hint this
         "kai_incident_store"
         # web.AppKey("kai_incident_store", IncidentStore)
     ].find_solutions(**params.model_dump())
 
     return web.json_response(
-        {
-            "solutions": [sol.model_dump_json() for sol in solutions],
-        }
+        ResponseGetSolutions(solutions=solutions).model_dump_json()
     )
 
 
