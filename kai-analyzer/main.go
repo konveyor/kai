86a4d832cbb75f71711acb60092c957b9617df47
David Zager <dzager@redhat.com>
2024-10-10 11:49:51 -0400

    :seedling: Be nice and provide configuration for log location
    Signed-off-by: David Zager <dzager@redhat.com>

--- kai-analyzer/main.go
+++ kai-analyzer/main.go
@@ -16,8 +16,11 @@ func main() {
 
 	sourceDirectory := flag.String("source-directory", ".", "This will be the absolute path to the source code directory that should be analyzed")
 	rulesDirectory := flag.String("rules-directory", ".", "This will be the absolute path to the rules directory")
+	logFile := flag.String("log-file", "", "This is the file where logs should be stored. By default they will just be written to stderr")
 	lspServerPath := flag.String("lspServerPath", "/Users/shurley/repos/kai/jdtls/bin/jdtls", "this will be the path to the lsp")
 	bundles := flag.String("bundles", "/Users/shurley/repos/MTA/java-analyzer-bundle/java-analyzer-bundle.core/target/java-analyzer-bundle.core-1.0.0-SNAPSHOT.jar", "this is the path to the java analyzer bundle")
+	// TODO(djzager): We should do verbosity type argument(s)
+	logLevel := -100
 
 	flag.Parse()
 	// In the future add cobra for flags maybe
@@ -31,9 +34,24 @@ func main() {
 		panic(fmt.Errorf("rules directory must be valid"))
 	}
 
-	logger := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
-		Level: slog.Level(-100),
-	})
+	// TODO(djzager): Handle log level/location more like reputable LSP servers
+	// ChatGPT told me gopls and jdtls default to stderr but I couldn't confirm that
+	// for now I think it's enough to make it configurable to be one or the other.
+	// The editor extension(s) can tee stderr to a temp file.
+	var logger *slog.JSONHandler
+	if logFile != nil && *logFile != "" {
+		file, err := os.Create(*logFile)
+		if err != nil {
+			panic(err)
+		}
+		logger = slog.NewJSONHandler(file, &slog.HandlerOptions{
+			Level: slog.Level(logLevel),
+		})
+	} else {
+		logger = slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+			Level: slog.Level(logLevel),
+		})
+	}
 
 	l := logr.FromSlogHandler(logger)
 	l.Info("Starting Analyzer", "source-dir", *sourceDirectory, "rules-dir", *rulesDirectory)
