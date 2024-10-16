1ce89339be0a24a86792a094c83730602155494a
github-action-benchmark <github@users.noreply.github.com>
2024-10-16 16:29:28 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for d755fa19fa0e74c292dd21e240394e4d78ba8090

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729096159135,
+  "lastUpdate": 1729096167993,
   "repoUrl": "https://github.com/konveyor/kai",
   "entries": {
     "Benchmark": [
@@ -11134,6 +11134,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_mistralai_mistral-7b-v0-1_example_a",
+            "value": "",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jwmatthews@gmail.com",
+            "name": "John Matthews",
+            "username": "jwmatthews"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d755fa19fa0e74c292dd21e240394e4d78ba8090",
+          "message": "Bump deps to address langchain openai warning (#401)\n\nSigned-off-by: John Matthews <jwmatthews@gmail.com>",
+          "timestamp": "2024-10-16T12:28:01-04:00",
+          "tree_id": "6f097289cfe7eabf712cc3e30e918f243819bf8c",
+          "url": "https://github.com/konveyor/kai/commit/d755fa19fa0e74c292dd21e240394e4d78ba8090"
+        },
+        "date": 1729096166434,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_meta-llama_llama-3-70b-instruct_example_a",
             "value": "",
             "unit": "Match"
           }
