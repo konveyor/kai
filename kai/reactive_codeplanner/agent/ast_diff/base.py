0d434b6e1d28668b12740643e53c94afba35fd3e
Fabian von Feilitzsch <fabian@fabianism.us>
2024-10-22 18:34:11 -0400

    I'm in heaven
    Signed-off-by: Fabian von Feilitzsch <fabian@fabianism.us>

--- kai/reactive_codeplanner/agent/ast_diff/base.py
+++ kai/reactive_codeplanner/agent/ast_diff/base.py
@@ -1,20 +1,16 @@
 from abc import ABC, abstractmethod
-from typing import Any, Iterator, Self, Generic
+from typing import Any, Iterator, Self
 
 from typing_extensions import TypeVar
 
 
-KT = TypeVar("KT")
-VT = TypeVar("VT", bound="DiffableSummary")
-
-
-class DiffableSummary[KT, VT: "DiffableSummary"](ABC):
+class DiffableSummary(ABC):
     """DiffableSummary is a class to store token information that can
     generate a diff with another version of same type of info
     """
 
     @abstractmethod
-    def to_dict(self) -> dict[KT, VT]:
+    def to_dict(self) -> dict[str, Any]:
         """Returns a dict representation of info
 
         Returns:
@@ -35,7 +31,11 @@ class DiffableSummary[KT, VT: "DiffableSummary"](ABC):
         pass
 
 
-class DiffableDict[KT, VT: DiffableSummary](dict[KT, VT], DiffableSummary):
+KT = TypeVar("KT")
+KV = TypeVar("KV", bound=DiffableSummary)
+
+
+class DiffableDict(dict[KT, KV], DiffableSummary):
     """A dict that's also a diffable, used to store nested tokens"""
 
     def __eq__(self, o: object) -> bool:
@@ -48,15 +48,13 @@ class DiffableDict[KT, VT: DiffableSummary](dict[KT, VT], DiffableSummary):
             return True
         return False
 
-    def iter_values(self) -> Iterator[VT]:
-        values: list[VT] = list(self.values())
-        for v in values:
-            yield v.to_dict()
+    def iter_values(self) -> Iterator[dict[str, Any]]:
+        return iter([v.to_dict() for _, v in self.items()])
 
-    def to_dict(self) -> dict[KT, VT]:
+    def to_dict(self) -> dict[str, Any]:
         return {str(k): v.to_dict() for k, v in self.items()}
 
-    def diff(self, o: Self) -> dict[KT, VT]:
+    def diff(self, o: Self) -> dict[str, Any]:
         diff = {}
         added = [o[key].to_dict() for key in set(o.keys()) - set(self.keys())]
         if added:
