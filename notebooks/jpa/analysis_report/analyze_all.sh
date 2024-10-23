e94a3a9bb39767eb8fbae1ea42996d912120b0eb
Pranav Gaikwad <pgaikwad@redhat.com>
2024-10-23 17:18:56 -0400

    Move samples/ to solution_server/ (#447)
    * :ghost: move samples to kai_solution_server/samples
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>
    
    * :ghost: gringo gogo
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>
    
    * :ghost: enchiladas
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>
    
    * m&ms
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>
    
    * :ghost: remove run_demo
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>
    
    ---------
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>
    Signed-off-by: Shawn Hurley <shawn@hurley.page>

--- notebooks/jpa/analysis_report/analyze_all.sh
+++ notebooks/jpa/analysis_report/analyze_all.sh
@@ -3,7 +3,7 @@
 apps=("cmt" "bmt" "greeter" "tasks-qute")
 
 for src_name in "${apps[@]}"; do
-	src_dir=${PWD}/../../../samples/sample_repos/${src_name}
+	src_dir=${PWD}/../../../kai_solution_server/samples/sample_repos/${src_name}
 	outdir=${PWD}/${src_name}
 	mkdir -p "${outdir}"
 	echo "Analyzing ${src_name} src: ${src_dir} out: ${outdir}"
@@ -12,5 +12,5 @@ for src_name in "${apps[@]}"; do
 	cd "${src_dir}" || exit
 	git checkout main
 	popd || exit
-	time ../../../samples/bin/kantra analyze -i "${src_dir}" -m source-only -t 'quarkus' -t 'jakarta-ee' -t 'jakarta-ee8+' -t 'jakarta-ee9+' -t 'cloud-readiness' --rules ./custom_rules -o "${outdir}" --overwrite
+	time ../../../kai_solution_server/samples/bin/kantra analyze -i "${src_dir}" -m source-only -t 'quarkus' -t 'jakarta-ee' -t 'jakarta-ee8+' -t 'jakarta-ee9+' -t 'cloud-readiness' --rules ./custom_rules -o "${outdir}" --overwrite
 done
