322fb6cff88aa18cda9d0598eece01433b5ffea5
Pranav Gaikwad <pgaikwad@redhat.com>
2024-10-23 12:02:42 -0400

    :ghost: gringo gogo
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- kai_solution_server/samples/macos/get_latest_kantra_cli.sh
+++ kai_solution_server/samples/macos/get_latest_kantra_cli.sh
@@ -2,5 +2,4 @@
 
 mkdir -p ../bin
 : ${TAG="latest"}
-podman pull quay.io/konveyor/kantra:$TAG && podman run --name kantra-download quay.io/konveyor/kantra:$TAG 1> /dev/null 2> /dev/null && podman cp kantra-download:/usr/local/bin/darwin-kantra ../bin/kantra && podman rm kantra-download
-
+podman pull quay.io/konveyor/kantra:"${TAG}" && podman run --name kantra-download quay.io/konveyor/kantra:"${TAG}" 1>/dev/null 2>/dev/null && podman cp kantra-download:/usr/local/bin/darwin-kantra ../bin/kantra && podman rm kantra-download
