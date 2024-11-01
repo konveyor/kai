95b73b930a44f6b53f0a6ced86cbb8dd83b2f14a
Shawn Hurley <shawn@hurley.page>
2024-11-01 15:23:18 -0400

    Moving maven compiler agent out, to a real agent and added tests
    Signed-off-by: Shawn Hurley <shawn@hurley.page>

--- kai/reactive_codeplanner/main.py
+++ kai/reactive_codeplanner/main.py
@@ -8,6 +8,7 @@ from kai.reactive_codeplanner.agent.dependency_agent.dependency_agent import (
     MavenDependencyAgent,
 )
 from kai.reactive_codeplanner.agent.analyzer_fix.agent import AnalyzerAgent
+from kai.reactive_codeplanner.agent.maven_compiler_fix.agent import MavenCompilerAgent
 from kai.reactive_codeplanner.task_manager.api import RpcClientConfig
 from kai.reactive_codeplanner.task_manager.task_manager import TaskManager
 from kai.reactive_codeplanner.task_runner.analyzer_lsp.task_runner import (
@@ -106,7 +107,7 @@ def main() -> None:
         RepoContextManager(config.repo_directory),
         None,
         validators=[MavenCompileStep(config), AnalyzerLSPStep(config)],
-        agents=[
+        task_runners=[
             DependencyTaskRunner(
                 MavenDependencyAgent(model_provider, config.repo_directory)
             ),
