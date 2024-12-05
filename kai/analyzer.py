d225b5b2c268c820c068747223e68787a83ec239
Pranav Gaikwad <pgaikwad@redhat.com>
2024-12-05 11:30:07 -0500

    Revert ":bug: Fix rulesets path in run_demo, remove unused workflows (#505)" (#511)
    This reverts commit 1f1baf1542c5c02961a65c4b5235880dad7c2e51.
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- kai/analyzer.py
+++ kai/analyzer.py
@@ -25,8 +25,7 @@ class AnalyzerLSP:
         rules_directory: Path,
         analyzer_lsp_path: Path,
         analyzer_java_bundle_path: Path,
-        dep_open_source_labels_path: Optional[Path] = None,
-        log_file: Optional[Path] = None,
+        dep_open_source_labels_path: Optional[Path],
     ) -> None:
         """This will start and analyzer-lsp jsonrpc server"""
         # trunk-ignore-begin(bandit/B603)
@@ -44,25 +43,19 @@ class AnalyzerLSP:
         if dep_open_source_labels_path is not None:
             args.append("-depOpenSourceLabelsFile")
             args.append(str(dep_open_source_labels_path))
-        server_stderr = subprocess.PIPE
-        if log_file is not None:
-            args.append("-log-file")
-            args.append(str(log_file))
-            server_stderr = subprocess.DEVNULL
         logger.debug(f"Starting analyzer rpc server with {args}")
         self.rpc_server = subprocess.Popen(
             args,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
-            stderr=server_stderr,
+            stderr=subprocess.PIPE,
         )
         # trunk-ignore-end(bandit/B603)
 
-        if log_file is None:
-            self.stderr_logging_thread = threading.Thread(
-                target=log_stderr, args=(self.rpc_server.stderr,)
-            )
-            self.stderr_logging_thread.start()
+        self.stderr_logging_thread = threading.Thread(
+            target=log_stderr, args=(self.rpc_server.stderr,)
+        )
+        self.stderr_logging_thread.start()
 
         self.rpc = JsonRpcServer(
             json_rpc_stream=BareJsonStream(
--- kai/reactive_codeplanner/main.py
+++ kai/reactive_codeplanner/main.py
@@ -141,7 +141,6 @@ def main() -> None:
             if args.dep_open_source_labels_path
             else None
         ),
-        log_file=Path(".", "kai-analyzer.log"),
     )
 
     task_manager = TaskManager(
