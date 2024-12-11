b45ded27ba9fd914ebe571380812518f72cc1c9d
JonahSussman <sussmanjonah@gmail.com>
2024-12-11 15:44:23 -0500

    Changed constants.py to use `pathlib.Path`
    Signed-off-by: JonahSussman <sussmanjonah@gmail.com>

--- kai/constants.py
+++ kai/constants.py
@@ -1,39 +1,40 @@
 import os
-import pathlib
 import sys
 from typing import Any
+from pathlib import Path
 
 """
 This file exists because we need to define some constants - specifically file
 paths - that are used in multiple places in the codebase. There might be a more
-robust solution, but for now, this should suffice
+robust solution, but for now, this should suffice.
+
+Note: We sometimes use `os.path.abspath` as opposed to `Path.resolve()` because
+the former will resolve relative paths, but not symlinks. This is in line with
+what Go does. 
 """
 
-PATH_KAI = os.path.dirname(os.path.abspath(__file__))
+PATH_KAI = Path(os.path.dirname(os.path.abspath(__file__)))
 
 # pyinstaller sets sys attributes to help determine when program runs in bin
 if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
-    PATH_KAI = sys._MEIPASS
+    PATH_KAI = Path(sys._MEIPASS)
 
-PATH_GIT_ROOT = os.path.join(PATH_KAI, "..")
+PATH_GIT_ROOT = Path(os.path.abspath(os.path.join(PATH_KAI, "..")))
 
-PATH_SOLUTION_SERVER_ROOT = os.path.join(PATH_GIT_ROOT, "kai_solution_server")
+PATH_SOLUTION_SERVER_ROOT = PATH_GIT_ROOT / "kai_solution_server"
 
-PATH_DATA = os.path.join(PATH_SOLUTION_SERVER_ROOT, "data")
+PATH_DATA = PATH_SOLUTION_SERVER_ROOT / "data"
 
-PATH_LLM_CACHE = os.path.join(PATH_KAI, "data", "llm_cache")
-PATH_BENCHMARKS = os.path.join(PATH_DATA, "benchmarks")
-PATH_MISC = os.path.join(PATH_DATA, "misc")
-PATH_SQL = os.path.join(PATH_DATA, "sql")
-PATH_TEMPLATES = os.path.join(PATH_DATA, "templates")
+PATH_LLM_CACHE = PATH_KAI / "data" / "llm_cache"
+PATH_BENCHMARKS = PATH_DATA / "benchmarks"
+PATH_MISC = PATH_DATA / "misc"
+PATH_SQL = PATH_DATA / "sql"
+PATH_TEMPLATES = PATH_DATA / "templates"
 
-PATH_LOCAL_REPO = os.path.join(
-    PATH_GIT_ROOT, "kai_solution_server/samples/sample_repos"
-)
+PATH_LOCAL_REPO = PATH_GIT_ROOT / "kai_solution_server" / "samples" / "sample_repos"
 
-PATH_TESTS = os.path.join(PATH_GIT_ROOT, "tests")
-PATH_TEST_DATA = pathlib.Path(os.path.join(PATH_GIT_ROOT, "tests/test_data"))
-
+PATH_TESTS = PATH_GIT_ROOT / "tests"
+PATH_TEST_DATA = PATH_GIT_ROOT / "tests" / "test_data"
 
 def __clean_env() -> dict[str, Any]:
     """
