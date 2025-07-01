61307258416911fbb6e843b004454522f46b7716
JonahSussman <sussmanjonah@gmail.com>
2025-06-30 21:26:59 -0400

    Added ast_diff
    Signed-off-by: JonahSussman <sussmanjonah@gmail.com>

---
+++ kai_mcp_solution_server/src/kai_mcp_solution_server/ast_diff/parser.py
@@ -0,0 +1,28 @@
+from enum import StrEnum
+
+import tree_sitter as ts
+import tree_sitter_java as ts_java
+
+from kai_mcp_solution_server.ast_diff.base import DiffableDict, DiffableSummary
+from kai_mcp_solution_server.ast_diff.java import _extract_java_info
+
+
+class Language(StrEnum):
+    Java = "java"
+    # TODO (pgaikwad): add a parser for xml
+    XML = "xml"
+
+
+def extract_ast_info(content: str, language: Language) -> DiffableSummary:
+    """
+    Returns a Diffable containing information about given AST node (usually root node of
+    a file)
+    """
+    match language:
+        case Language.Java:
+            return _extract_java_info(
+                ts.Parser(ts.Language(ts_java.language())).parse(
+                    content.encode("utf-8")
+                )
+            )
+    return DiffableDict()
