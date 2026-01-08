00bfeb9e439ad0dc1bb16e8bcc9a1a08cf4730f6
Savitha Raghunathan <6026527+savitharaghunathan@users.noreply.github.com>
2026-01-08 09:48:37 -0500

    :ghost: remove check for pipename (#912)
    * add check for csharp pipename
    
    Signed-off-by: Savitha Raghunathan <saveetha13@gmail.com>
    
    * remove pipename check
    
    Signed-off-by: Savitha Raghunathan <saveetha13@gmail.com>
    
    ---------
    
    Signed-off-by: Savitha Raghunathan <saveetha13@gmail.com>

--- kai_analyzer_rpc/pkg/service/pipe_analyzer.go
+++ kai_analyzer_rpc/pkg/service/pipe_analyzer.go
@@ -62,9 +62,6 @@ func NewPipeAnalyzer(ctx context.Context, limitIncidents, limitCodeSnips, contex
 			return nil, fmt.Errorf("you can only use an existing provider serving at a particular location")
 		}
 		for _, initConfig := range config.InitConfig {
-			if initConfig.PipeName == "" {
-				return nil, fmt.Errorf("the providers should only be using a pipe to communicate to the LSP")
-			}
 			locations = append(locations, initConfig.Location)
 			defaultBuiltinConfigs = append(defaultBuiltinConfigs, provider.InitConfig{
 				Location: initConfig.Location,
